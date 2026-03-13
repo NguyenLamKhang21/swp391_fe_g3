938e9b81544fc180b9bb9f6c9abdb8e3bba6caa2
Hưng Nguyễn <warriror21@gmail.com>
2026-03-13 08:00:42 +0700

    update api for food management in central kitchen

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -31,4 +31,10 @@ export const updateOrderPriority = (orderId, newPriority, note = "") =>
 
 export const getCentralKitchenFood = () => API.get("/central_foods");
 
+export const getCentralFoodById = (id) => API.get(`/central_foods/${id}`);
+
+export const updateCentralFood = (id, data) => API.put(`/central_foods/${id}`, data);
+
+export const deleteCentralFood = (id) => API.delete(`/central_foods/${id}`);
+
 export const getOrderByStatus = (status) => API.get(`/orders/status/${status}`);
